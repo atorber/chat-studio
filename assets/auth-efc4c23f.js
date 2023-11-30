f9fdc204c13f855840643cad677d54e28412073a
atorber <atorber@users.noreply.github.com>
2023-11-30 03:28:10 +0000

    deploy: c4b30643d0f70b13b7791c6e486eb473fadd2e89

--- assets/auth-efc4c23f.js
+++
@@ -1,1 +0,0 @@
-import{ck as s,p as r}from"./index-6fac5f50.js";const o=e=>s(e),t=e=>r("/api/v1/auth/register",e),i=e=>r("/api/v1/auth/forget",e);export{o as S,t as a,i as b};
---
+++ assets/auth-f70dec43.js
@@ -0,0 +1,1 @@
+import{dA as e}from"./index-6a9cab4f.js";const a=r=>e("/api/v1/auth/login",r),o=r=>e("/api/v1/auth/register",r),s=r=>e("/api/v1/auth/forget",r);export{a as S,o as a,s as b};
