6d351f9343f91548f9b7289e02402776f0717dda
atorber <atorber@163.com>
2023-12-19 20:12:58 +0800

    0.6.5增加Copilot助手UI

--- src/views/message/index.vue
+++ src/views/message/index.vue
@@ -4,6 +4,7 @@ import { useDialogueStore } from '@/store'
 import IndexContent from './inner/IndexContent.vue'
 import IndexSider from './inner/IndexSider.vue'
 import IndexAmicable from './inner/IndexAmicable.vue'
+import Chat from '../../components/Chat/index.vue'
 
 const dialogueStore = useDialogueStore()
 
@@ -32,6 +33,13 @@ onUnmounted(() => {
     <main class="el-main">
       <component :is="indexName ? IndexContent : IndexAmicable" />
     </main>
+
+    <!-- <div style="width: 450px; background-color: aliceblue; text-align: center; padding-top: 100px">
+      <div>Chat Copilot操作区</div>
+    </div> -->
+    <div style="width: 420px; background-color: aliceblue; text-align: center;">
+      <Chat @close="true" @updateApiKey="true" @logout="onUnmounted"></Chat>
+    </div>
   </section>
 </template>
 
